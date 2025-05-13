70702f4d2adaa2c93e646e625d0eed76cf475c15
Daniel B <Dragon863.dev@gmail.com>
2025-05-13 23:14:24 +0100

    Add experience tab, UI tweaks and improvements

---
+++ content/experience.md
@@ -0,0 +1,5 @@
+---
+title: "Experience"
+url: "/experience"
+layout: experience
+---
--- themes/dragon863/static/js/light.js
+++ themes/dragon863/static/js/light.js
@@ -6,8 +6,8 @@ function navigateToGithub() {
     window.open("https://github.com/Dragon863", "_blank");
 }
 
-function navigateToBlog() {
-    document.location = "/posts";
+function navigateToLinkedIn() {
+    document.location = "https://www.linkedin.com/in/daniel-benge-672176365/";
 }
 
 function mailTo() {
